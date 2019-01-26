b79fada649d2b6d55537e1252d3461111985392e
Sergey Vidyuk <sir.vestnik@gmail.com>
2019-01-26 22:54:21 +0700

    BIGMIN implementation with not yet fixed bugs

--- mapex/morton_code.hpp
+++ mapex/morton_code.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <cassert>
 #include <cstdint>
+#include <numeric>
 
 struct point {
   uint32_t x = 0;
@@ -9,6 +11,8 @@ struct point {
 
 constexpr bool operator==(point l, point r) noexcept { return l.x == r.x && l.y == r.y; }
 constexpr bool operator!=(point l, point r) noexcept { return l.x != r.x || l.y != r.y; }
+
+namespace morton {
 
 namespace detial {
 
@@ -42,5 +46,54 @@ constexpr uint32_t deinterleave(uint64_t val) noexcept {
   return static_cast<uint32_t>(val);
 }
 
-constexpr uint64_t morton_code(point pt) noexcept { return interleave(pt.x) | (interleave(pt.y) << 1); }
-constexpr point from_morton(uint64_t code) noexcept { return {deinterleave(code), deinterleave(code >> 1)}; }
+constexpr uint64_t code(point pt) noexcept { return interleave(pt.x) | (interleave(pt.y) << 1); }
+constexpr point decode(uint64_t code) noexcept { return {deinterleave(code), deinterleave(code >> 1)}; }
+
+namespace detail {
+
+constexpr uint64_t load_masks[] = {interleave(uint32_t{1} << 31) << 1, interleave(~(uint32_t{1} << 31)) << 1};
+constexpr uint64_t untoched_bits_mask = interleave(~uint32_t{0}) << 1;
+constexpr unsigned u64_bits_count = 64;
+
+// Operation actually stores certain value in the current axis bits but `load` name is used in bigmin/litmax algorythms
+// explanation in the original work introducing them: Tropf, H.; Herzog, H. (1981), "Multidimensional Range Search in
+// Dynamically Balanced Trees", Angewandte Informatik, 2: 71–77.
+constexpr uint64_t load(uint64_t mask, unsigned bit_pos, uint64_t code) noexcept {
+  mask = mask >> (u64_bits_count - 1 - bit_pos);
+  const uint64_t zero_bits_reset = code & ((untoched_bits_mask >> (bit_pos % 2)) | mask);
+  return zero_bits_reset | mask;
+}
+
+} // namespace detail
+
+constexpr uint64_t bigmin(uint64_t division_point, uint64_t min, uint64_t max) noexcept {
+  assert(division_point <= max);
+  assert(min <= max);
+  // min and max must be codes of rect corners with minimal coordinate values and maximal coordinate values.
+  assert(decode(min).x <= decode(max).x);
+  assert(decode(min).y <= decode(max).y);
+  uint64_t res = max;
+  for (unsigned bit_pos = detail::u64_bits_count - 1; bit_pos < detail::u64_bits_count; --bit_pos) {
+    const uint64_t examined_bits =
+        (((division_point >> bit_pos) & 1) << 2) | (((min >> bit_pos) & 1) << 1) | ((max >> bit_pos) & 1);
+    switch (examined_bits) {
+    case 0b000:
+    case 0b111:
+      break;
+    case 0b001:
+      res = detail::load(detail::load_masks[0], bit_pos, min);
+      max = detail::load(detail::load_masks[1], bit_pos, max);
+      break;
+    case 0b101:
+      min = detail::load(detail::load_masks[0], bit_pos, min);
+      break;
+    case 0b011:
+      return min;
+    case 0b100:
+      return res;
+    }
+  }
+  return res;
+}
+
+} // namespace morton
--- mapex/morton_code.test.cpp
+++ mapex/morton_code.test.cpp
@@ -4,6 +4,30 @@
 
 namespace {
 
+static_assert(morton::detail::load(morton::detail::load_masks[0], 2, 0b0000) == 0b0100,
+    "load('100...', val) should set value when all bits are zero");
+static_assert(morton::detail::load(morton::detail::load_masks[1], 5, 0b000000) == 0b001010,
+    "load('011...', val) should set value when all bits are zero");
+static_assert(morton::detail::load(morton::detail::load_masks[0], 3, 0b1010) == 0b1000,
+    "load('100...', val) should set value when all bits are one");
+static_assert(morton::detail::load(morton::detail::load_masks[1], 4, 0b010101) == 0b000101,
+    "load('011...', val) should set value when all bits are one");
+static_assert(morton::detail::load(morton::detail::load_masks[0], 1, 0b1101) == 0b0111,
+    "load('100...', val) do not affect untoched axis");
+static_assert(morton::detail::load(morton::detail::load_masks[1], 0, 0b111011) == 0b101010,
+    "load('011...', val) do not affect untoched axis");
+
+static_assert(
+    morton::bigmin(0, 0, 0) == 0, "bigmin should work on single point rect in the top left of coordinate space");
+static_assert(morton::bigmin(0, ~uint64_t{0}, ~uint64_t{0}) == ~uint64_t{0},
+    "bigmin should work on single point rect in the bottom right of coordinate space");
+static_assert(morton::bigmin(0, morton::code({1, 1}), morton::code({3, 5})) == morton::code({1, 1}),
+    "bigmin should return top left of the rect for point with code below code of the rect top left");
+
+constexpr bool is_in_rect(point pt, point rect_min, point rect_max) noexcept {
+  return pt.x >= rect_min.x && pt.y >= rect_min.y && pt.x <= rect_max.x && pt.y <= rect_max.y;
+}
+
 // clang-format off
 constexpr uint32_t values[] = {
     0b0000'0000, 0b0000'0001, 0b0000'0010, 0b0000'0011, 0b0000'0100, 0b0000'0101, 0b0000'0110, 0b0000'0111,
@@ -34,6 +58,29 @@ private:
     }
   }
 
+  void random_rects_with_div_points(unsigned count) {
+    QTest::addColumn<uint64_t>("div_pt");
+    QTest::addColumn<uint64_t>("min");
+    QTest::addColumn<uint64_t>("max");
+
+    std::uniform_int_distribution<uint32_t> coord_dist;
+    std::uniform_int_distribution<uint64_t> code_dist;
+    for (unsigned i = 0; i < count; ++i) {
+      const point min{coord_dist(rnd_engine_), coord_dist(rnd_engine_)};
+      point max;
+      coord_dist.param(std::uniform_int_distribution<uint32_t>::param_type{min.x});
+      max.x = coord_dist(rnd_engine_);
+      coord_dist.param(std::uniform_int_distribution<uint32_t>::param_type{min.y});
+      max.y = coord_dist(rnd_engine_);
+      code_dist.param(std::uniform_int_distribution<uint64_t>::param_type{morton::code(min), morton::code(max)});
+      const uint64_t div_pt = code_dist(rnd_engine_);
+      QTest::addRow("bigmin(%llX, %llX, %llX)", static_cast<unsigned long long>(div_pt),
+          static_cast<unsigned long long>(morton::code(min)), static_cast<unsigned long long>(morton::code(max)))
+          << div_pt << morton::code(min) << morton::code(max);
+      coord_dist.param(std::uniform_int_distribution<uint32_t>::param_type{});
+    }
+  }
+
 private slots:
   void encode_and_decode_point_is_identity_data() {
     QTest::addColumn<uint32_t>("x");
@@ -50,14 +97,14 @@ private slots:
   void encode_and_decode_point_is_identity() {
     QFETCH(uint32_t, x);
     QFETCH(uint32_t, y);
-    QCOMPARE(from_morton(morton_code({x, y})), (point{x, y}));
+    QCOMPARE(morton::decode(morton::code({x, y})), (point{x, y}));
   }
 
   void groups_of_4_bits_interleaved_correctly_data() { all_4bit_groups(); }
   void groups_of_4_bits_interleaved_correctly() {
     QFETCH(uint32_t, group_pos);
     QFETCH(size_t, group_item);
-    QCOMPARE(interleave(values[group_item] << (group_sz * group_pos)),
+    QCOMPARE(morton::interleave(values[group_item] << (group_sz * group_pos)),
         interleaved[group_item] << (2 * group_sz * group_pos));
   }
 
@@ -65,12 +112,33 @@ private slots:
   void groups_of_4_bits_deinterleaved_correctly() {
     QFETCH(uint32_t, group_pos);
     QFETCH(size_t, group_item);
-    QCOMPARE(deinterleave(interleaved[group_item] << (2 * group_sz * group_pos)),
+    QCOMPARE(morton::deinterleave(interleaved[group_item] << (2 * group_sz * group_pos)),
         values[group_item] << (group_sz * group_pos));
+  }
+
+  void bigmin_result_is_inside_the_rect_data() { random_rects_with_div_points(100); }
+  void bigmin_result_is_inside_the_rect() {
+    QFETCH(uint64_t, div_pt);
+    QFETCH(uint64_t, min);
+    QFETCH(uint64_t, max);
+    const point bigmin = morton::decode(morton::bigmin(div_pt, min, max));
+    QVERIFY(is_in_rect(bigmin, morton::decode(min), morton::decode(max)));
+  }
+
+  void no_rect_points_between_div_pt_and_bigmin_data() { random_rects_with_div_points(10); }
+  void no_rect_points_between_div_pt_and_bigmin() {
+    QFETCH(uint64_t, div_pt);
+    QFETCH(uint64_t, min);
+    QFETCH(uint64_t, max);
+    const uint64_t bigmin = morton::bigmin(div_pt, min, max);
+    for (uint64_t pt = div_pt; pt < bigmin; ++pt)
+      QVERIFY(!is_in_rect(morton::decode(pt), morton::decode(min), morton::decode(max)));
   }
 
 private:
   std::default_random_engine rnd_engine_;
+  const point rect_min_ = {100, 500};
+  const point rect_max_ = {rect_min_.x + 640, rect_min_.y + 480};
 };
 
 QTEST_MAIN(morton_code_tests)
